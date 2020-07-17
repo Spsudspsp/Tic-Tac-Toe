eaf7734f76613eae70d9e9d04c75c320d90a214c
Spsudspsp <68426394+Spsudspsp@users.noreply.github.com>
2020-07-17 05:08:12 -0700

    Update game.py

--- game.py
+++ game.py
@@ -9,21 +9,28 @@ while True:
         board[row][col] = 'X'
         p_board = f'{board[0]}\n{board[1]}\n{board[2]}'
 
-
+# rows
         r1x = True if board[0][0] == "X" and board[0][1] == 'X' and board[0][2] == 'X' else False
         r2x = True if board[1][0] == "X" and board[1][1] == 'X' and board[1][2] == 'X' else False
         r3x = True if board[2][0] == "X" and board[2][1] == 'X' and board[2][2] == 'X' else False
-        c1x = True if board[0][0] == 'X' and board[1][0] == 'X' and board[2][0] == 'X' else False
-        c2x = True if board[0][1] == 'X' and board[1][1] == 'X' and board[2][1] == 'X' else False
-        c3x = True if board[0][2] == 'X' and board[1][2] == 'X' and board[2][2] == 'X' else False
-        d1x = True if board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X' else False
-        d2x = True if board[0][2] == 'X' and board[1][1] == 'X' and board[2][1] == 'X' else False
+        
         r1o = True if board[0][0] == "O" and board[0][1] == 'O' and board[0][2] == 'O' else False
         r2o = True if board[1][0] == "O" and board[1][1] == 'O' and board[1][2] == 'O' else False
         r3o = True if board[2][0] == "O" and board[2][1] == 'O' and board[2][2] == 'O' else False
+        
+# columns
+        c1x = True if board[0][0] == 'X' and board[1][0] == 'X' and board[2][0] == 'X' else False
+        c2x = True if board[0][1] == 'X' and board[1][1] == 'X' and board[2][1] == 'X' else False
+        c3x = True if board[0][2] == 'X' and board[1][2] == 'X' and board[2][2] == 'X' else False
+        
         c1o = True if board[0][0] == 'O' and board[1][0] == 'O' and board[2][0] == 'O' else False
         c2o = True if board[0][1] == 'O' and board[1][1] == 'O' and board[2][1] == 'O' else False
         c3o = True if board[0][2] == 'O' and board[1][2] == 'O' and board[2][2] == 'O' else False
+        
+# diagonals
+        d1x = True if board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X' else False
+        d2x = True if board[0][2] == 'X' and board[1][1] == 'X' and board[2][1] == 'X' else False
+         
         d1o = True if board[0][0] == 'O' and board[1][1] == 'O' and board[2][2] == 'O' else False
         d2o = True if board[0][2] == 'O' and board[1][1] == 'O' and board[2][1] == 'O' else False
 
